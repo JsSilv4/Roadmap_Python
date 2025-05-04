82217835f90d8e8b8949b273997879cbe0789b68
Jefferson Santos <jeff3rs0n@icloud.com>
2025-05-03 22:24:31 -0400

    HelloWorld

---
+++ Basic_Syntax/HelloWorld.py
@@ -0,0 +1,17 @@
+#iniciando a maquina virtual
+# python3 -m venv venv
+
+# ativando a maquina virtual
+# windows
+# venv\Scripts\activate
+
+# linux
+# source venv/bin/activate
+
+
+#Iniciando o github
+# git init
+# 
+
+
+print("Hello World")
